ba77e78275fc0a2f01605ccc70d936f4ca94c889
CepCap <nikinoob@gmail.com>
2018-10-17 06:48:24 -0700

    Database migration

---
+++ db/migrate/20181017133001_create_categories.rb
@@ -0,0 +1,10 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.integer :level, null: false
+      t.string :title, null: false
+
+      t.timestamps
+    end
+  end
+end
