4af973fb7a55855d248a250e95eef30db786e6be
CepCap <nikinoob@gmail.com>
2018-11-03 15:58:03 -0700

    minor corrections and locale update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
-  has_many :created_tests, class_name 'Test', foreign_key: :author_id
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
 
   def passed_tests_by_level(level)
     tests.where(level: level)
