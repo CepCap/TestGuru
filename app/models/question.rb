5a6d26cfffc2a1b3cec060a3eeea2e4b4c6be8c4
CepCap <nikinoob@gmail.com>
2018-11-08 10:28:06 -0800

    Corrections

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
+
+  validates :body, presence: true
 end
--- db/migrate/20181030224605_add_progress_to_tests_users.rb
+++ db/migrate/20181030224605_add_progress_to_tests_users.rb
@@ -1,6 +1,5 @@
 class AddProgressToTestsUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :tests_users, :progress, :integer, default: 0
-    add_column :tests_users, :creator, :boolean
   end
 end
