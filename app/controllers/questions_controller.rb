9b94df81479b6e14f71c0ce92b22a833ea8fd0aa
CepCap <nikinoob@gmail.com>
2018-11-13 07:39:24 -0800

    Corrections

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,32 @@
 class QuestionsController < ApplicationController
-  
+  before_action :find_question, only: [:create, :index, :show, :destroy]
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def create
-    question = Question.create(question_params)
+    question = Question.create(question_params) unless question_params == nil
     question.test_id = params[:test_id]
     render plain:question.inspect
     # redirect_to controller: :tests, action: "show", id: params[:test_id] 
   end
 
   def index
-    render inline: "<p> <%= Question.where(test_id: params[:test_id]).collect(&:body).join(' | ') %> </p>"
+    render inline: "<p> <%= @question.pluck(:body).join(' | ') %> </p>"
   end
 
   def show
-    render inline: "<p> <%= Question.find(params[:id]).body %> </p>"
+    render inline: "<p> <%= @question.body %> </p>"
   end
 
   def destroy
-    Question.find(params[:id]).destroy
+    @question.destroy
   end
 
   private
+
+  def find_question
+    @question = Test.find(params[:test_id]).questions
+  end
 
   def question_params
     params.require(:question).permit(:body)
