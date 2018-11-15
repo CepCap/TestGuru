7eaa3d57f22d0643d06b9fb917eff60ba9e2fd00
CepCap <nikinoob@gmail.com>
2018-11-15 02:41:18 -0800

    Corrections

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,11 @@
 class QuestionsController < ApplicationController
   before_action :current_test, only: [:index, :create]
-  before_action :current_test_questions, only: [:index, :create]
   before_action :standalone_question, only: [:show, :destroy]
   
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def create
-    question = Question.new(question_params)
-    question.test_id = @current_test.id
+    question = @current_test.questions.new(question_params)
     if question.save 
       render plain: question.inspect
     else
@@ -20,22 +18,18 @@ class QuestionsController < ApplicationController
   end
 
   def index
-    render inline: "<p> <%= @current_test_questions.pluck(:body).join(' | ') %> </p>"
+    render inline: "<p> <%= @current_test.questions.pluck(:body).join(' | ') %> </p>"
   end
 
   def destroy
     @standalone_question.destroy
-    redirect_to controller: :tests, action: "show", id: params[:test_id]
+    redirect_to test_path(@current_test)
   end
 
   private
 
   def current_test
     @current_test = Test.find(params[:test_id])
-  end
-
-  def current_test_questions
-    @current_test_questions = @current_test.questions
   end
 
   def standalone_question
@@ -47,6 +41,7 @@ class QuestionsController < ApplicationController
   end
 
   def rescue_with_question_not_found
+    render plain: "Question not found"
     head 404
   end
 end
