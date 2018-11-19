4885443295e35437b4d781c5db0b7ea4c833c1d4
CepCap <nikinoob@gmail.com>
2018-11-19 14:44:08 -0800

    Adding views, layouts. Overall corrections

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,39 +1,44 @@
 class QuestionsController < ApplicationController
-  before_action :current_test, only: [:index, :create]
-  before_action :standalone_question, only: [:show, :destroy]
+  before_action :find_test, only: [:index, :create]
+  before_action :find_question, only: [:show, :destroy]
   
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
+  def new
+    @question = Question.new
+  end
+
   def create
-    question = @current_test.questions.new(question_params)
-    if question.save 
-      render plain: question.inspect
-    else
-      render plain: "Failed to save question"
-    end
+    # question = @find_test.questions.new(question_params)
+    @question = Question.new(question_params)
+    # if @question.save 
+    #   redirect_to @question
+    # else
+    #   render plain: "Failed to save question"
+    # end
   end
 
   def show
-    render inline: "<p> <%= @standalone_question.body %> </p>"
+    render inline: "<p> <%= @find_question.body %> </p>"
   end
 
   def index
-    render inline: "<p> <%= @current_test.questions.pluck(:body).join(' | ') %> </p>"
+    render inline: "<p> <%= @find_test.questions.pluck(:body).join(' | ') %> </p>"
   end
 
   def destroy
-    @standalone_question.destroy
-    redirect_to test_path(@current_test)
+    @find_question.destroy
+    redirect_to test_path(@find_test)
   end
 
   private
 
-  def current_test
-    @current_test = Test.find(params[:test_id])
+  def find_test
+    @find_test = Test.find(params[:test_id])
   end
 
-  def standalone_question
-    @standalone_question = Question.find(params[:id])
+  def find_question
+    @find_question = Question.find(params[:id])
   end
 
   def question_params
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,15 +2,26 @@ class TestsController < ApplicationController
   before_action :find_test, only: %i[show]
 
   def index
-    render inline: '<p>All tests: </p> <%= Test.all.pluck(:title).join(" | ") %>'
+    @tests = Test.all
   end
 
   def show
-    render inline: '<p> <%= @test.title %> </p>
-                   <body> <%= @test.questions.pluck(:body).join(" | ") %> </body>'
+    @test = Test.find(params[:id])
+  end
+
+  def create
+    @test = Test.new(test_params)
+  end 
+
+  def new
+    @test = Test.new
   end
 
   private 
+
+  def test_params
+
+  end
 
   def find_test
     @test = Test.find(params[:id])
