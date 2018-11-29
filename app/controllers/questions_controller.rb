38eff51dfe954c5ad5f5d10bf75e7f79b2dc4a71
CepCap <nikinoob@gmail.com>
2018-11-29 16:50:39 +0300

    Revert "Adding views, particles"

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,50 +1,39 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:create, :new]
-  before_action :find_question, only: [:show, :destroy, :edit, :update]
+  before_action :current_test, only: [:index, :create]
+  before_action :standalone_question, only: [:show, :destroy]
   
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def new
-    @question = @test.questions.new
-  end
-
   def create
-    @question = Question.new(question_params)
-    @question.test_id = params[:test_id]
-    if @question.save 
-      redirect_to @question
+    question = @current_test.questions.new(question_params)
+    if question.save 
+      render plain: question.inspect
     else
-      render :new
+      render plain: "Failed to save question"
     end
   end
 
   def show
+    render inline: "<p> <%= @standalone_question.body %> </p>"
+  end
+
+  def index
+    render inline: "<p> <%= @current_test.questions.pluck(:body).join(' | ') %> </p>"
   end
 
   def destroy
-    @question.delete
-    redirect_to test_path(@question.test)
-  end
-
-  def edit
-  end
-
-  def update
-    if @question.update(question_params)
-      redirect_to @question
-    else
-      render :edit
-    end
+    @standalone_question.destroy
+    redirect_to test_path(@current_test)
   end
 
   private
 
-  def find_test
-    @test = Test.find(params[:test_id])
+  def current_test
+    @current_test = Test.find(params[:test_id])
   end
 
-  def find_question
-    @question = Question.find(params[:id])
+  def standalone_question
+    @standalone_question = Question.find(params[:id])
   end
 
   def question_params
