2d7236570f71a8198a6a54c77d40678c8905d70d
CepCap <nikinoob@gmail.com>
2019-01-31 11:42:26 -0800

    Corrections

--- app/assets/javascripts/utilities/test_progression.js
+++ app/assets/javascripts/utilities/test_progression.js
@@ -1,14 +1,19 @@
 document.addEventListener('turbolinks:load', function() {
   var myProgress = document.querySelector('.myProgress')
   var completion = myProgress.dataset.progress
-  myProgress.onload = move(completion)
-})
-
-
-function move(completion) {
+  // myProgress.onload = move(completion)
   var myBar = document.querySelector('.myBar')
   myBar.style.width = completion + '%'
   if (completion > 0) {
     myBar.classList.remove('hide')
   }
-}
+})
+
+// 
+// function move(completion) {
+//   var myBar = document.querySelector('.myBar')
+//   myBar.style.width = completion + '%'
+//   if (completion > 0) {
+//     myBar.classList.remove('hide')
+//   }
+// }
