9dbb6e633fa64876062706b93687d56d902b000c
CepCap <nikinoob@gmail.com>
2019-01-31 11:50:29 -0800

    corrections

--- app/assets/javascripts/utilities/test_progression.js
+++ app/assets/javascripts/utilities/test_progression.js
@@ -1,4 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
+  var myProgress = document.querySelector('.myProgress')
   var completion = myProgress.dataset.progress
   var myBar = document.querySelector('.myBar')
   myBar.style.width = completion + '%'
