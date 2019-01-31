a53f980049f0b0204cfe364aea84fdf9b83d8112
CepCap <nikinoob@gmail.com>
2019-01-31 11:46:18 -0800

    Corrections

--- app/assets/javascripts/utilities/test_progression.js
+++ app/assets/javascripts/utilities/test_progression.js
@@ -1,19 +1,8 @@
 document.addEventListener('turbolinks:load', function() {
-  var myProgress = document.querySelector('.myProgress')
   var completion = myProgress.dataset.progress
-  // myProgress.onload = move(completion)
   var myBar = document.querySelector('.myBar')
   myBar.style.width = completion + '%'
   if (completion > 0) {
     myBar.classList.remove('hide')
   }
 })
-
-// 
-// function move(completion) {
-//   var myBar = document.querySelector('.myBar')
-//   myBar.style.width = completion + '%'
-//   if (completion > 0) {
-//     myBar.classList.remove('hide')
-//   }
-// }
