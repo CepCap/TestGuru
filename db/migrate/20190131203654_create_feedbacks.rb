1ecc5cf58591cc933c2875cf8e7430dd3c65bbfe
CepCap <nikinoob@gmail.com>
2019-02-02 00:52:56 -0800

    Corrections

--- db/migrate/20190131203654_create_feedbacks.rb
+++ db/migrate/20190131203654_create_feedbacks.rb
@@ -2,7 +2,7 @@ class CreateFeedbacks < ActiveRecord::Migration[5.2]
   def change
     create_table :feedbacks do |t|
       t.string :message
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: { to_table :users }
 
       t.timestamps
     end
