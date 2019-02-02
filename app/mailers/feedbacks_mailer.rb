8a94cf4e5f7bb06ef1800ada89d06dc30d419878
CepCap <nikinoob@gmail.com>
2019-02-02 01:15:38 -0800

    Corrections

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,8 +1,8 @@
 class FeedbacksMailer < ApplicationMailer
+  default from: ENV['SMTP_USERNAME']
 
   def send_feedback(feedback)
     @message = feedback.message
-    @admin_mail = ENV['SMTP_USERNAME']
-    mail(to: @admin_mail, from: @admin_mail, subject: 'Feedback from Testguru')
+    mail(to: ENV['SMTP_USERNAME'], subject: 'Feedback from Testguru')
   end
 end
