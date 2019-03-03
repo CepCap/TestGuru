9d45babaa6b1aa0c9e98ee1c532076d5b66b7b4c
CepCap <nikinoob@gmail.com>
2019-03-03 11:40:37 -0800

    Adding timer to tests

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -54,7 +54,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :time)
   end
 
   def find_test
